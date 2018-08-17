597a4749babffa9cb52d5bc4370ff9f789f961b3
Leonardo Lima <leonardoresendelima@gmail.com>
2018-08-17 06:59:47 -0300

    Ajustes Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,21 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
 import { NewAccountComponent } from './new-account/new-account.component';
+import { AccountsService } from './accounts.service';
+import { LoggingService } from './logging.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AccountComponent,
-    NewAccountComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		AccountComponent,
+		NewAccountComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		HttpModule
+	],
+	providers: [AccountsService, LoggingService],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
